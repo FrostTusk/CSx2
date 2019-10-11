7a4d35e2f8a38731141dafde044f9deb3d78ca96
FrostTusk <mathijs.hubrechtsen@student.kuleuven.be>
2019-10-11 20:39:39 +0200

    started refactoring

--- ical.py
+++ ical.py
@@ -2,6 +2,11 @@ import urllib.request
 import datetime, pytz, sys
 import os.path
 
+# --- Constants
+TARGET_URL = 'https://people.cs.kuleuven.be/~btw/roosters1920/cws_semester_1.html'
+OUT_FILENAME = 'events.ical'
+# ---
+
 # python3 ical.py <textfile, EXACT names of courses>
 filter = []
 if len(sys.argv) == 2 and os.path.isfile(sys.argv[0]):
@@ -13,13 +18,45 @@ for i in range(len(filter)):
     filter[i] = filter[i].strip()
 
 
-byte_contents = urllib.request.urlopen("https://people.cs.kuleuven.be/~btw/roosters1920/cws_semester_1.html").read()
+byte_contents = urllib.request.urlopen(TARGET_URL).read()
 contents = byte_contents.decode('utf-8')
+cursor = contents.find('<hr>') # Skip to first week
 
-cursor = contents.find('<hr>')
-
-out = open('events.ics', 'w+')
+out = open(OUT_FILENAME, 'w+')
 out.write('BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//MCWS Classes//EN\n')
+
+def get_date(week):
+    date = week[week.find('<b>')+3:week.find('</b>')-5]
+    date = date.split(' ')[1].split('.')
+    return datetime.date(year=int(date[2]), month=int(date[1]), day=int(date[0]))
+
+
+class Event:
+    # Sets all the atributes of the Event
+    # and moves the daycursor to the next Event in that day.
+    def parse_in_event(self, week, daycursor):
+        time = week[daycursor+8:week.find('</td>',daycursor)]
+        starttime = datetime.datetime.strptime(time.split('&#8212;')[0], '%H:%M').time()
+        endtime = datetime.datetime.strptime(time.split('&#8212;')[1], '%H:%M').time()
+
+        self.startdt = datetime.datetime.combine(date=date, time=starttime, tzinfo=pytz.timezone('Europe/Brussels'))
+        self.enddt = datetime.datetime.combine(date=date, time=endtime, tzinfo=pytz.timezone('Europe/Brussels'))
+
+        daycursor = week.find('<td>in</td>',daycursor+1)
+        self.location = week[daycursor+20:week.find('</td>', daycursor+20)].replace(' '*5,' ')
+
+        daycursor = week.find('>', week.find('<font', daycursor)) + 1
+        self.name = week[daycursor:week.find('</font>', daycursor+1)]
+        return week.find('<tr><td>', daycursor + 1)
+
+    def write_to_file(self, out):
+        out.write('BEGIN:VEVENT\n')
+        out.write('DTSTART:' + str(self.startdt.strftime('%Y%m%dT%H%M%S')) + '\n')
+        out.write('DTEND:' + str(self.enddt.strftime('%Y%m%dT%H%M%S')) + '\n')
+        out.write('LOCATION:' + self.location + '\n')
+        out.write('SUMMARY:' + self.name + '\n')
+        out.write('END:VEVENT\n')
+
 
 while(cursor > 0):
     weekstart = cursor
@@ -29,41 +66,15 @@ while(cursor > 0):
 
     week = contents[weekstart:weekend]
 
-    # get the date
-    date = week[week.find('<b>')+3:week.find('</b>')-5]
-    date = date.split(' ')[1].split('.')
-    date = datetime.date(year=int(date[2]), month=int(date[1]), day=int(date[0]))
+    date = get_date(week)
 
     #while for classes
     daycursor = week.find('<tr><td>')
     while(daycursor > 0):
-        time = week[daycursor+8:week.find('</td>',daycursor)]
-        starttime = datetime.datetime.strptime(time.split('&#8212;')[0], '%H:%M').time()
-        endtime = datetime.datetime.strptime(time.split('&#8212;')[1], '%H:%M').time()
-
-        startdt = datetime.datetime.combine(date=date, time=starttime, tzinfo=pytz.timezone('Europe/Brussels'))
-        enddt = datetime.datetime.combine(date=date, time=endtime, tzinfo=pytz.timezone('Europe/Brussels'))
-
-        daycursor = week.find('<td>in</td>',daycursor+1)
-        location = week[daycursor+20:week.find('</td>', daycursor+20)].replace(' '*5,' ')
-
-        daycursor = week.find('>', week.find('<font', daycursor)) + 1
-        name = week[daycursor:week.find('</font>', daycursor+1)]
-        daycursor = week.find('<tr><td>', daycursor + 1)
-
-        print(name)
-        print(filter)
-        if len(filter) != 0 and name.strip() not in filter:
-            continue
-
-        out.write('BEGIN:VEVENT\n')
-        out.write('DTSTART:' + str(startdt.strftime('%Y%m%dT%H%M%S')) + '\n')
-        out.write('DTEND:' + str(enddt.strftime('%Y%m%dT%H%M%S')) + '\n')
-        out.write('LOCATION:' + location + '\n')
-        out.write('SUMMARY:' + name + '\n')
-        out.write('END:VEVENT\n')
-
-
+        event = Event()
+        daycursor = event.parse_in_event(week, daycursor)
+        if len(filter) == 0 or name.strip() in filter:
+            event.write_to_file(out)
 
     cursor = contents.find('<hr>', cursor + 1)
 
